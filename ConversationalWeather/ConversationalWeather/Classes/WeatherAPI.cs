2d5e69b02b66dd13e15859f3b68e7b392aeb2a39
Dirk Songuer <dirk@songuer.de>
2016-03-27 19:04:23 +0200

    Initial import

---
+++ ConversationalWeather/ConversationalWeather/Classes/WeatherAPI.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+using Windows.Web.Http;
+using Windows.Web.Http.Filters;
+using Windows.Security.Cryptography.Certificates;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ConversationalWeather.Objects;
+
+namespace ConversationalWeather.WeatherAPI
+{
+    public class WeatherInterface : INotifyPropertyChanged
+    {
+        // property changed callback
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // cancellation token for aborting geolocation search
+        private CancellationTokenSource _cts = null;
+
+        // http filter and client
+        private HttpBaseProtocolFilter filter;
+        private HttpClient httpClient;
+
+        // flag to indicate if geolocation is locked / known
+        public Boolean geopositionLocked = false;
+
+        // contains the found geolocation
+        private Geoposition _currentPosition;
+
+        // cotnains the status of the geolocation tracking
+        private String _status = "";
+
+        // object containing the returned weather data
+        private WeatherForecastObject _weatherForecast = new WeatherForecastObject();
+
+        // constructor
+        public WeatherInterface()
+        {
+            geopositionLocked = false;
+
+            // create an HttpClient instance with default settings. I.e. no custom filters. 
+            filter = new HttpBaseProtocolFilter();
+            httpClient = new HttpClient(filter);
+        }
+
+        // setter / getter for the current position
+        public Geoposition CurrentPosition
+        {
+            get { return _currentPosition; }
+            set
+            {
+                // we have a geolocation locked now
+                geopositionLocked = true;
+
+                // store the data
+                _currentPosition = value;
+
+                this.GetWeatherForcastForGeoposition();
+
+                // call the OnPropertyChanged callback to indicate new data
+                this.OnPropertyChanged("CurrentPosition");
+            }
+        }
+
+        // setter / getter for the status
+        public String Status
+        {
+            get { return _status; }
+            set
+            {
+                // store the data
+                _status = value;
+
+                // call the OnPropertyChanged callback to indicate new data
+                this.OnPropertyChanged("Status");
+            }
+        }
+
+        // setter / getter for the weather data response
+        public WeatherForecastObject WeatherForecast
+        {
+            get { return _weatherForecast; }
+            set
+            {
+                // store the data
+                _weatherForecast = value;
+
+                // call the OnPropertyChanged callback to indicate new data
+                this.OnPropertyChanged("WeatherForecast");
+            }
+        }
+
+        // create the OnPropertyChanged method to raise the event
+        protected void OnPropertyChanged(string name)
+        {
+            // get the handler for the event
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                // if somebody subscribed, send the event
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        // get the current user location
+        // this will use Windows.Devices.Geolocation to query the current location 
+        async public void GetCurrentLocation()
+        {
+            try
+            {
+                Status = "Trying to get your location";
+
+                // request permission to access location
+                var accessStatus = await Geolocator.RequestAccessAsync();
+
+                // user chose if access to geolocation features is granted
+                switch (accessStatus)
+                {
+                    // in case the user allowed it, start searching for the location
+                    case GeolocationAccessStatus.Allowed:
+
+                        // get cancellation token
+                        _cts = new CancellationTokenSource();
+                        CancellationToken token = _cts.Token;
+
+                        // if DesiredAccuracy or DesiredAccuracyInMeters are not set (or value is 0), DesiredAccuracy.Default is used.
+                        Geolocator geolocator = new Geolocator { DesiredAccuracyInMeters = 0 };
+
+                        // try to get the geolocation and set it to the property
+                        CurrentPosition = await geolocator.GetGeopositionAsync().AsTask(token);
+                        Status = "Ok, found you!";
+                        break;
+
+                    // if the user denied the access
+                    case GeolocationAccessStatus.Denied:
+                        geopositionLocked = false;
+                        Status = "Access to location is denied.";
+                        break;
+
+                    // if something else went wrong
+                    case GeolocationAccessStatus.Unspecified:
+                        geopositionLocked = false;
+                        Status = "Unspecified geolocation tracking error.";
+                        break;
+                }
+            }
+            // user cancelled request
+            catch (TaskCanceledException)
+            {
+                geopositionLocked = false;
+                Status = "Geolocation tracking cancelled.";
+            }
+            // exception was caught
+            catch (Exception ex)
+            {
+                geopositionLocked = false;
+                Status = "General error when tracking geolocation: " + ex.ToString();
+            }
+            // done
+            finally
+            {
+                geopositionLocked = false;
+                _cts = null;
+            }
+        }
+
+        // get the current user location
+        // this will use Windows.Devices.Geolocation to query the current location 
+        async public void GetWeatherForcastForGeoposition()
+        {
+            try
+            {
+                // create URL for getting forcast
+                String weatherApiUrl = "http://api.openweathermap.org/data/2.5/forecast";
+                weatherApiUrl += "?lat=" + CurrentPosition.Coordinate.Latitude.ToString();
+                weatherApiUrl += "&lon=" + CurrentPosition.Coordinate.Longitude.ToString();
+                weatherApiUrl += "&APPID=60c2636cd48cde2176af74be0d397d00";
+
+                // set status to loading
+                Status = "Loading weather data";
+
+                // load weather forcast data
+                Uri myUri = new Uri(weatherApiUrl, UriKind.Absolute);
+                HttpResponseMessage response = await httpClient.GetAsync(myUri).AsTask(_cts.Token);
+
+                // convert json data to object
+                WeatherForecast = JsonConvert.DeserializeObject<WeatherForecastObject>(response.Content.ToString());
+            }
+            // exception was caught
+            catch (Exception ex)
+            {
+                Status = "A problem occured during loading and converting the weather data: " + ex.ToString();
+            }
+        }
+    }
+}
---
+++ ConversationalWeather/ConversationalWeather/MainPage.xaml.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.ViewManagement;
+using ConversationalWeather.WeatherAPI;
+using Windows.Storage;
+
+// Die Vorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 dokumentiert.
+
+namespace ConversationalWeather
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        WeatherInterface weatherApi = new WeatherInterface();
+
+        Dictionary<int, Image> weatherIconDictionary = new Dictionary<int, Image>();
+
+        public MainPage()
+        {
+            // initialise component
+            this.InitializeComponent();
+
+            // add callback events for weather api
+            weatherApi.PropertyChanged += weatherApiPropertyChanged;
+
+            // create dictionary for images
+            // this is basically a fake list
+            weatherIconDictionary.Add(1, imageWeatherIcon1);
+            weatherIconDictionary.Add(2, imageWeatherIcon2);
+            weatherIconDictionary.Add(3, imageWeatherIcon3);
+            weatherIconDictionary.Add(4, imageWeatherIcon4);
+
+            // set icon size to quarter of the screen width
+            Windows.Foundation.Rect bounds = ApplicationView.GetForCurrentView().VisibleBounds;
+            imageWeatherIcon1.Width = (bounds.Width / 4.5);
+            imageWeatherIcon2.Width = (bounds.Width / 4.5);
+            imageWeatherIcon3.Width = (bounds.Width / 4.5);
+            imageWeatherIcon4.Width = (bounds.Width / 4.5);
+
+            // get current geolocation
+            // note that his will trigger a weather forecast for this location
+            weatherApi.GetCurrentLocation();
+        }
+
+        public void weatherApiPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            // position was found
+            if (e.PropertyName == "CurrentPosition")
+                GeopositionFound();
+
+            // forecast was load
+            if (e.PropertyName == "WeatherForecast")
+                WeatherForecastLoaded();
+
+            // status was changed
+            if (e.PropertyName == "Status")
+                StatusChanged();
+        }
+
+        public void GeopositionFound()
+        {
+            // textWeatherInformation.Text = "Loading some weather information";
+        }
+
+        // weather data was received
+        // it's now available in weatherApi.WeatherForecast
+        public void WeatherForecastLoaded()
+        {
+            // show location string
+            textLocationInformation.Text = "Seems like you're in " + weatherApi.WeatherForecast.city.name.ToString();
+            String weatherSummary = "There will be ";
+
+            // get dictionary to fill all forecasted weather states
+            Dictionary<string, int> weatherDictionary = new Dictionary<string, int>();
+            // iterate through all forecasted weather nodes
+            for (int i = 0; i < weatherApi.WeatherForecast.list.Count; i++)
+            {
+                // check if the weather state is already known
+                if (!weatherDictionary.ContainsKey(weatherApi.WeatherForecast.list[i].weather[0].description.ToString()))
+                {
+                    // if not known yet, add the weather state to the dictionary
+                    weatherDictionary.Add(weatherApi.WeatherForecast.list[i].weather[0].description.ToString(), weatherApi.WeatherForecast.list[i].weather[0].id);
+                }
+            }
+
+            // create a local counter
+            int j = 1;
+
+            // loop through all found weather states
+            foreach (KeyValuePair<string, int> pair in weatherDictionary)
+            {
+                // increase the counter
+                // note that this runs one in front of the item count
+                j++;
+
+                // store the weather state in the summary string
+                weatherSummary += pair.Key;
+
+                // check if this is an item in the middle
+                if (j < weatherDictionary.Count)
+                {
+                    weatherSummary += ", ";
+                }
+                // or the last item in the dictionary
+                else if (j == weatherDictionary.Count)
+                {
+                    weatherSummary += " and then ";
+                }
+
+                // the first 4 items should display a matching icon
+                if (j <= 5)
+                {
+                    this.selectWeatherIcon(pair.Value.ToString(), (j - 1), false);
+                }
+            }
+
+            // display the aggregated weather summary
+            textWeatherInformation.Text = weatherSummary + " later.";
+        }
+
+        // this will load a weather icon
+        // the weather code will contain the code from openweathermap
+        // the icon index is the reference to the weather icon dictionary
+        // recursion is a flag to avoid infinite loops by indicating if the icon has been called recusively or not
+        async public void selectWeatherIcon(String weatherCode, int weatherIconIndex, Boolean recursion)
+        {
+            try
+            {
+                // try to load the file
+                // note that if the file does not exist, an exception will be thrown
+                StorageFile item = await StorageFile.GetFileFromApplicationUriAsync(new Uri(this.BaseUri, "/Assets/WeatherIcons/" + weatherCode + ".png"));
+
+                // here we can assume that the file does exist
+                // we create a bitmap with the icon and load it into the respective image icon slot 
+                // https://msdn.microsoft.com/en-us/windows/uwp/porting/wpsl-to-uwp-namespace-and-class-mappings
+                BitmapImage bitmapImage = new BitmapImage(new Uri(this.BaseUri, "/Assets/WeatherIcons/" + weatherCode + ".png"));
+                weatherIconDictionary[weatherIconIndex].Source = bitmapImage;
+            }
+            catch (FileNotFoundException ex)
+            {
+                if (!recursion)
+                {
+                    DateTime currentTime = DateTime.Now;
+
+                    if ((currentTime.Hour > 8) && (currentTime.Hour < 20))
+                    {
+                        this.selectWeatherIcon(weatherCode + "_day", weatherIconIndex, true);
+                    }
+                    else
+                    {
+                        this.selectWeatherIcon(weatherCode + "_night", weatherIconIndex, true);
+                    }
+                }
+                else
+                {
+                    string[] stringSeparators = new string[] { "_" };
+                    string[] result;
+
+                    // Display the original string and delimiter string.
+                    // Split a string delimited by another string and return all elements.
+                    result = weatherCode.Split(stringSeparators, StringSplitOptions.None);
+
+                    int rawWeatherCode = Convert.ToInt16(result[0]);
+                    int baseWeatherCode = Convert.ToInt16(rawWeatherCode / 100) * 100;
+
+                    if (baseWeatherCode != rawWeatherCode)
+                    {
+                        this.selectWeatherIcon(baseWeatherCode.ToString(), weatherIconIndex, false);
+                    }
+                }
+            }
+        }
+
+        public void StatusChanged()
+        {
+            textLocationInformation.Text = weatherApi.Status;
+        }
+
+    }
+}
